a29768121a34fa9810e11c6e83f7edbe28ed9ad0
ramoncarrascom <ramon@ramoncarrasco.es>
2020-03-20 23:30:50 +0100

    ADDED Grid scaffolding

---
+++ TicTacToe/Data/IGrid.cs
@@ -0,0 +1,40 @@
+﻿using BoringGames.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Data
+{
+    /// <summary>
+    /// Contract for grid
+    /// </summary>
+    public interface IGrid
+    {
+        /// <summary>
+        /// Sets the value for a Grid's position
+        /// </summary>
+        /// <param name="xCoord">X coordinate (horizontal position) from 0 to 2</param>
+        /// <param name="yCoord">Y coordinate (vertical position) from 0 to 2</param>
+        /// <param name="player">Player making the movement</param>
+        /// <returns>Next suggested player</returns>
+        public void Set(int xCoord, int yCoord, CellPlayer player);
+
+        /// <summary>
+        /// Initializes grid
+        /// </summary>
+        public void Init();
+
+        /// <summary>
+        /// Gets the current grid
+        /// </summary>
+        /// <returns>Returns a copy of the current grid</returns>
+        public ICell[][] GetGrid();
+
+        /// <summary>
+        /// Checks if a player has won the game
+        /// </summary>
+        /// <returns>Value of the winner. Returns CellPlayer.NONE if there's still no winner.</returns>
+        /// <exception cref="GameOverException">Raised when all movements have been made</exception>
+        public CellPlayer Check();
+    }
+}
---
+++ TicTacToe/Data/Implementation/Grid.cs
@@ -0,0 +1,30 @@
+﻿using BoringGames.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Data.Implementation
+{
+    public class Grid : IGrid
+    {
+        public CellPlayer Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICell[][] GetGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(int xCoord, int yCoord, CellPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
