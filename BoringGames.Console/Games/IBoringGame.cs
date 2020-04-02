2c98f133bde38a8aa18e6c19c35f9b5170aa292e
ramoncarrascom <ramon@ramoncarrasco.es>
2020-04-02 19:40:29 +0200

    ADDED Main game and first usable version

---
+++ BoringGames.Console/Games/IBoringGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoringGames.Txt.Games
+{
+    /// <summary>
+    /// Interface for Boring Games
+    /// </summary>
+    public interface IBoringGame
+    {
+        /// <summary>
+        /// Runs the game
+        /// </summary>
+        void Run();
+    }
+}
---
+++ BoringGames.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using BoringGames.Core.Exceptions;
+using BoringGames.Txt.Games;
+using System;
+
+namespace BoringGames.Txt
+{
+    /// <summary>
+    /// Main program
+    /// </summary>
+    class Program
+    {
+        private IBoringGame game;
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.Welcome();
+
+            try
+            {
+                p.game = new BoringToe();
+                p.game.Run();
+            } catch (BgException bge)
+            {
+                Console.WriteLine("BoringGamesException - " + bge.Message);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Exception - " + e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Welcome message
+        /// </summary>
+        private void Welcome()
+        {
+            Console.WriteLine("Welcome to Boring Games 0.1");
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
