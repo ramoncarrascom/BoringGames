f82492262a28e5a21a50c5c8c5c6f2abbc55189a
ramoncarrascom <ramon@ramoncarrasco.es>
2020-05-10 18:40:37 +0200

    FIX Css

--- BoringGames.Web/boring-games/src/app/games/shared/playerdisplay/playerdisplay.component.ts
+++ BoringGames.Web/boring-games/src/app/games/shared/playerdisplay/playerdisplay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'shared-playerdisplay',
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playerdisplay.component.scss'],
 })
 
-export class PlayerDisplayComponent implements OnInit {
+export class PlayerDisplayComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  /**
+   * Player's reference
+   * @example 'Player A'
+   */
+  @Input() PlayerReference: string;
+
+  /**
+   * Player's name
+   * @example 'Player A Name'
+   */
+  @Input() PlayerName: string;
+
+  public playerReference: string;
+  public playerName: string;
+
+  /**
+   * Constructor
+   */
+  constructor() {
+    this.playerName = 'PlayerName';
+    this.playerReference = 'Player A';
+  }
+
+  /**
+   * OnChanges implementation
+   * @param changes Changes
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    this.checkPlayerReference(changes);
+    this.checkPlayerName(changes);
+  }
 
   ngOnInit() {}
+
+  /**
+   * Checks if Player Reference has changed. If so, it changes the Player's reference
+   * @param changes Object from ngOnChanges
+   */
+  private checkPlayerReference(changes: SimpleChanges) {
+    if (changes && changes.PlayerReference && changes.PlayerReference.currentValue) {
+      this.playerReference = changes.PlayerReference.currentValue;
+    }
+  }
+
+  /**
+   * Checks if Player Name has changed. If so, it changes the Player's name
+   * @param changes Object from ngOnChanges
+   */
+  private checkPlayerName(changes: SimpleChanges) {
+    if (changes && changes.PlayerName && changes.PlayerName.currentValue) {
+      this.playerName = changes.PlayerName.currentValue;
+    }
+  }
 
 }
