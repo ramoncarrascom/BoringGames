cceb52c853f9d74c8385fc9432ecb5a4e9eb3a95
ramoncarrascom <ramon@ramoncarrasco.es>
2020-05-27 22:25:54 +0200

    ADDED Cell unit tests

--- BoringGames.Web/boring-games/src/app/games/boringtoe/cell/cell.component.spec.ts
+++ BoringGames.Web/boring-games/src/app/games/boringtoe/cell/cell.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { CellComponent } from './cell.component';
+import { SimpleChange } from '@angular/core';
+import { element } from 'protractor';
 
 describe('CellComponent', () => {
   let component: CellComponent;
@@ -20,5 +22,42 @@ describe('CellComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnChanges must set Cells value', () => {
+
+    it('if input is A, then Cell must be X', () => {
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, 'A', false)
+      });
+      fixture.detectChanges();
+
+      const myDiv = fixture.debugElement.nativeElement.querySelector('#cell-data');
+      expect(myDiv.innerHTML).toBe(' X ');
+    });
+
+    it('if input is B, then Cell must be Y', () => {
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, 'B', false)
+      });
+      fixture.detectChanges();
+
+      const myDiv = fixture.debugElement.nativeElement.querySelector('#cell-data');
+      expect(myDiv.innerHTML).toBe(' O ');
+    });
+
+    it('if input is <space>, then Cell must be <space>', () => {
+
+      component.ngOnChanges({
+        data: new SimpleChange(null, ' ', false)
+      });
+      fixture.detectChanges();
+
+      const myDiv = fixture.debugElement.nativeElement.querySelector('#cell-data');
+      expect(myDiv.innerHTML).toBe('   ');
+    });
+
   });
 });
